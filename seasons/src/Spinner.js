a79089a82d0776fd6793a880ee2c5cd38120e2b8
emmafattori <emmalfattori@gmail.com>
2021-02-15 11:47:16 -0500

    finishing seasons React Project

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Spinner = (props) => {
+	return (
+		<div>
+				<div class="ui active dimmer">
+					<div class="ui big text loader">{props.message}</div>
+				</div>
+				<p></p>
+		</div>
+	)
+}
+
+Spinner.defaultProps = {
+	message: 'Loading...'
+};
+export default Spinner;
---
+++ seasons/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner.js'
+
+class App extends React.Component{
+	
+	state = {lat: null, errorMessage: ''};
+
+	componentDidMount() {
+		
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({
+					lat: position.coords.latitude
+				})	
+			}, // success callback
+			(err) => {
+				this.setState({
+					errorMessage: err.message
+				})
+			} //fail or error callback
+		);
+	}
+
+	renderContent = () => {
+		if (this.state.errorMessage && !this.state.lat){
+			return <div>Error: {this.state.errorMessage} </div>
+		}
+		if (!this.state.errorMessage && this.state.lat){
+			return <SeasonDisplay lat={this.state.lat} />
+		}
+		return <Spinner message="Please accept location request"/>
+	}
+	
+	// you always need a render method in React
+	render() {
+		return (
+			<div className="border red">
+				{this.renderContent()}
+			</div>
+		)
+
+		
+	}
+}
+
+
+ReactDom.render(
+	<App />, 
+	document.querySelector('#root')
+)
